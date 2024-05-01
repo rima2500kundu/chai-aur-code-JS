8a6c56ef7777a74a3b53bf67532cceb80c1ad3ac
Rima Kundu <rimasdgworks@gmail.com>
2024-05-01 12:52:01 +0000

    added files

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict" // treat all JS code as newer version
+// alert(3+3); // we are using nodejs, not browser.
+
+let name = "Rima";
+let age = 10;
+let isLoggedIn = true;
+
+/*
+
+Primitive Data Types:
+number, string, bigint, boolean, null, undefined, symbol => unique
+
+object
+
+*/
+
+console.log(typeof null); // type is object.
