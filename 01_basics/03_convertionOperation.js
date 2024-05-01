16b2c07a8dd56fd873dca466da5a498c9a6ce480
Rima Kundu <rimasdgworks@gmail.com>
2024-05-01 14:02:53 +0000

    all file modified

--- 01_basics/03_convertionOperation.js
+++ 01_basics/03_convertionOperation.js
@@ -29,5 +29,40 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 
 let someNumber = 33;
 let stringNumber = String(someNumber);
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+/****************Operation***************/
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+let str1 = "Hello"
+let str2 = " Rima"
+let str3 = str1 + str2
+// console.log(str1 + str2);
+// console.log(str3);
+
+// console.log(true);
+
+// let gameCounter = 100;
+// ++gameCounter;
+// console.log(gameCounter);
+
+// prefix & postfix increments
+let x = 100;
+// let y = x++;
+let y = ++x;
+// console.log(y);
+
+// prefix & postfix decrements
+let a = 50;
+// let b = a++;
+let b = ++a;
+console.log(b);
+
+
+
+
+
+
