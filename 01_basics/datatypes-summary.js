1101222c7159e2a286ead0e93ffe1269a23d2d48
Rima Kundu <rimasdgworks@gmail.com>
2024-05-02 05:14:53 +0000

    added files

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,35 @@
+// Reference / Non-Primitive Datatypes
+// Array, Objects, Functions
+
+const score = 100;
+const scoreValue = 100.3;
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+// console.log(id === anotherId);
+
+// const bigNumber = 5686464646565464654566n;
+
+const heros = ["shaktiman", "naagraj", "doga"]; // arrays
+let myObj = { // object
+    name: Rima,
+    age: 10,
+}
+
+const myFunction = function(){ // function
+    console.log("Hello world");
+}
+
+console.log(typeof bigNumber);
+
+
+
+
+
+
+
+
+
